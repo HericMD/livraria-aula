b58d0fb27c1f4afd7cce6860b95d79bbe8fa99d5
HericMD <hericmdms@gmail.com>
2023-06-16 10:54:58 -0300

    autenticação

--- livraria/views.py
+++ livraria/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
 
 from livraria.models import Autor, Categoria, Editora, Livro
 from livraria.serializers import AutorSerializer, CategoriaSerializer, EditoraSerializer, LivroSerializer, LivroDetailSerializer
@@ -12,6 +13,7 @@ class AutorViewSet(ModelViewSet):
 class CategoriaViewSet(ModelViewSet):
     queryset = Categoria.objects.all()
     serializer_class = CategoriaSerializer
+    #permission_classes = [IsAuthenticated]
 
 class EditoraViewSet(ModelViewSet):
     queryset = Editora.objects.all()
