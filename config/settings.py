9392faaaaa91272da92812eaa4c6cf0509084426
HericMD <hericmdms@gmail.com>
2023-06-23 10:24:25 -0300

    Autenticação com o SimpleJWT

--- config/settings.py
+++ config/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "corsheaders",
     "rest_framework",
     "livraria",
+    "rest_framework_simplejwt",
 ]
 
 MIDDLEWARE = [
@@ -132,5 +133,8 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         #"rest_framework.permissions.IsAuthenticated",
         "rest_framework.permissions.DjangoModelPermissions",
-    ]
+    ],
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
 }
