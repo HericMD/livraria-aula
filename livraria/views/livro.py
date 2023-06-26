f12a13d37668309b819f81a087417db8864a6531
HericMD <hericmdms@gmail.com>
2023-06-26 13:29:56 -0300

    Reestrutaração-corrigida

--- livraria/views/livro.py
+++ livraria/views/livro.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 
 from livraria.models import Livro
-from livraria.serializers import LivroSerializer
+from livraria.serializers import LivroSerializer, LivroDetailSerializer
 
 class LivroViewSet(ModelViewSet):
     queryset = Livro.objects.all()
