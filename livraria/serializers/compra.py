b8f9daac58025cf0c53e2a85c02cd17960fa541b
-global <-global>
2023-09-04 16:55:12 -0300

    #28 - itenscompra endpoint (incompleto)

--- livraria/serializers/compra.py
+++ livraria/serializers/compra.py
@@ -1,8 +1,15 @@
 from rest_framework.serializers import ModelSerializer, CharField
 
-from livraria.models import Compra
+from livraria.models import Compra, ItensCompra
+
+class ItensCompraSerializer(ModelSerializer):
+    class Meta:
+        model = ItensCompra
+        fields = ["livro", "quantidade"]
+        depth = 1
 
 class CompraSerializer(ModelSerializer):
+    itens = ItensCompraSerializer(many=True, read_only=True)
     class Meta:
         model = Compra
-        fields = ["id", "usuario", "status"]
+        fields = ["id", "usuario", "status", "itens"]
